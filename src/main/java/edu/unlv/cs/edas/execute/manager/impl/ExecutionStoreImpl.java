8687d902f630a1740de2c876d90e54605a31fb2a
Chris Jones <chris@balln.net>
2014-06-01 13:24:39 -0700

    Fixes an issue where session execution caches were not cleared when multiple executions referenced the same graph or algorithm.

--- src/main/java/edu/unlv/cs/edas/execute/manager/impl/ExecutionStoreImpl.java
+++ src/main/java/edu/unlv/cs/edas/execute/manager/impl/ExecutionStoreImpl.java
@@ -4,7 +4,9 @@ import static org.springframework.context.annotation.ScopedProxyMode.INTERFACES;
 
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.bson.types.ObjectId;
 import org.springframework.context.annotation.Scope;
@@ -19,8 +21,8 @@ public class ExecutionStoreImpl implements ExecutionsStore, Serializable {
 	private static final long serialVersionUID = 1547827486004092536L;
 
 	private Map<ObjectId, Execution> executions = new HashMap<>();
-	private Map<ObjectId, ObjectId> executionIdsByAlgorithmId = new HashMap<>();
-	private Map<ObjectId, ObjectId> executionIdsByGraphDetailsId = new HashMap<>();
+	private Map<ObjectId, Set<ObjectId>> executionIdsByAlgorithmId = new HashMap<>();
+	private Map<ObjectId, Set<ObjectId>> executionIdsByGraphDetailsId = new HashMap<>();
 	
 	@Override
 	public Execution get(ObjectId id) {
@@ -35,8 +37,22 @@ public class ExecutionStoreImpl implements ExecutionsStore, Serializable {
 	@Override
 	public void put(ObjectId id, Execution execution) {
 		executions.put(id, execution);
-		executionIdsByAlgorithmId.put(execution.getAlgorithm().getId(), id);
-		executionIdsByGraphDetailsId.put(execution.getDesignGraphDetails().getId(), id);
+		
+		ObjectId algorithmId = execution.getAlgorithm().getId();
+		Set<ObjectId> algorithmIdSet = executionIdsByAlgorithmId.get(algorithmId);
+		if (algorithmIdSet == null) {
+			algorithmIdSet = new HashSet<>();
+			executionIdsByAlgorithmId.put(algorithmId, algorithmIdSet);
+		}
+		algorithmIdSet.add(id);
+		
+		ObjectId graphId = execution.getDesignGraphDetails().getId();
+		Set<ObjectId> graphIdSet = executionIdsByGraphDetailsId.get(graphId);
+		if (graphIdSet == null) {
+			graphIdSet = new HashSet<>();
+			executionIdsByGraphDetailsId.put(graphId, graphIdSet);
+		}
+		graphIdSet.add(id);
 	}
 
 	@Override
@@ -53,14 +69,20 @@ public class ExecutionStoreImpl implements ExecutionsStore, Serializable {
 	@Override
 	public void removeByAlgorithmId(ObjectId algorithmId) {
 		if (executionIdsByAlgorithmId.containsKey(algorithmId)) {
-			remove(executionIdsByAlgorithmId.get(algorithmId));
+			Set<ObjectId> executionIds = executionIdsByAlgorithmId.get(algorithmId);
+			for (ObjectId executionId : executionIds) {
+				remove(executionId);
+			}
 		}
 	}
 	
 	@Override
 	public void removeByGraphDetailsId(ObjectId graphDetailsId) {
 		if (executionIdsByGraphDetailsId.containsKey(graphDetailsId)) {
-			remove(executionIdsByGraphDetailsId.get(graphDetailsId));
+			Set<ObjectId> executionIds = executionIdsByGraphDetailsId.get(graphDetailsId);
+			for (ObjectId executionId : executionIds) {
+				remove(executionId);
+			}
 		}
 	}
 	
